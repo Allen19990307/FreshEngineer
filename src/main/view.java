4b0a3f0f813cede84a067096efe5411ecafa9891
Allen19990307 <63383505+Allen19990307@users.noreply.github.com>
2022-01-09 19:59:52 +0800

    1.初级功能，登录页面

---
+++ src/main/view.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * @Author:Allen
+ * @Descrition:
+ * @Date:1/9/2022 7:48 PM
+ */
+public class view {
+    public static void main(String[] args) {
+        System.out.println("欢迎来到okr的挑战世界");
+        System.out.println("当你决心有所成就的时候，请输入大写的OKR:  ");
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+
+        if("OKR".equals(next)){
+            System.out.println("首先我们需要确定一些规则");
+        }else{
+            System.out.println("追求卓越方有传奇,我们高处相见");
+        }
+
+
+    }
+}
